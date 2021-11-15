7cb179c1e229e9eb18fb2e0766a2c1c011049d76
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-11-15 22:06:35 +0600

    all are completed

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
 //monogdb Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@noyonecommerce.qnayd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
@@ -23,13 +22,8 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
-
-
-
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -62,6 +56,31 @@ async function run() {
       const drone = await droneCollection.findOne({ _id: ObjectId(id) })
       res.json(drone)
     })
+    // delete drone
+    app.delete('/drones/:id', async (req, res) => {
+      const result = await droneCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      })
+      res.send(result)
+    })
+
+    //Update drone information
+    app.put('/drones/:id', async (req, res) => {
+      const id = req.params.id
+      const updateTplaceInfo = req.body
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          name: updateTplaceInfo.name,
+          description: updateTplaceInfo.description,
+          image: updateTplaceInfo.image,
+          price: updateTplaceInfo.price,
+        },
+      }
+      const result = await droneCollection.updateOne(filter, updateDoc, options)
+      res.json(result)
+    })
 
     //post order information
     app.post('/orders', async (req, res) => {
@@ -70,6 +89,21 @@ async function run() {
       res.json(newOrder)
     })
 
+    // //GET allOrders
+    
+    // GET allOrders
+    app.get('/allOrders', async (req, res) => {
+      const result = await orderCollection.find({}).toArray()
+      res.json(result)
+    })
+
+    //Delete ordery from all Orders
+    app.delete('/orders/:id', async (req, res) => {
+      const result = await orderCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      })
+      res.send(result)
+    })
     // my Orders
     app.get('/myOrder/:email', async (req, res) => {
       const result = await orderCollection
@@ -88,6 +122,28 @@ async function run() {
       res.json(result)
     })
 
+    // delete allOrder
+    // app.delete('/allOrders/:id', async (req, res) => {
+    //   const result = await orderCollection.deleteOne({
+    //     _id: ObjectId(req.params.id),
+    //   })
+    //   res.send(result)
+    // })
+   // Update panding state
+    app.put('/allOrders/:id', async (req, res) => {
+      const id = req.params.id
+      const updateTplaceInfo = req.body
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          pending: updateTplaceInfo.pending,
+        },
+      }
+      const result = await orderCollection.updateOne(filter, updateDoc, options)
+      res.json(result)
+    })
+
     //post review information
     app.post('/reviews', async (req, res) => {
       const review = req.body
@@ -100,10 +156,36 @@ async function run() {
       const reviews = await reviewCollection.find().toArray()
       res.json(reviews)
     })
+
+    //users collections
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const result = await usersCollection.insertOne(user)
+      res.json(result)
+    })
+    //admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body
+      const filter = { email: user.email }
+      const updateDoc = { $set: { role: 'admin' } }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.json(result)
+    })
+
+    //admin get
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      let isAdmin = false
+      if (user?.role === 'admin') {
+        isAdmin = true
+      }
+      res.json({ admin: isAdmin })
+    })
   } finally {
     // await client.close();
   }
 }
 
 run().catch(console.dir)
-
